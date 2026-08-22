c893cc53580fab59b5b73c59ecde742aec724f24
John Doe <johndoe@example.com>
2026-08-22 20:03:07 +0700

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
+
 <template>
-  <h1>Index page</h1>
+  <div>
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
+  </div>
 </template>
